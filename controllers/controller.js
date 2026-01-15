0018344d47cb7e78a91a19170b75340d2e27d643
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-15 21:50:27 +0700

    R3: create routres

---
+++ controllers/controller.js
@@ -0,0 +1,53 @@
+class Controller {
+    static async readMemes(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+    static async readCategories(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+    static async detailMeme(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+    static async showForm(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+    static async postAdd(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+    static async vote(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+    static async funny(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+            res.send(error);
+        }
+    }
+}
+
+module.exports = Controller;
--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,18 @@
+const Controller = require("../controllers/controller");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send(`Init routers`);
-});
+//read
+router.get("/", Controller.readMemes);
+router.get("/categories", Controller.readCategories);
+router.get("/categories/:id", Controller.detailMeme);
+
+//add
+router.get("/memes/add", Controller.showForm);
+router.post("/memes/add", Controller.postAdd);
+
+//edit
+router.get("/memes/:id/vote", Controller.vote);
+router.get("/memes/:id/funny", Controller.funny);
 
 module.exports = router;
