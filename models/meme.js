6201fc796689f3325824ba3db0cc2af77987152e
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-15 21:41:28 +0700

    R1: declare assiciation

--- models/meme.js
+++ models/meme.js
@@ -1,27 +1,30 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Meme extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Meme extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Meme.belongsTo(models.Category);
+        }
     }
-  }
-  Meme.init({
-    author: DataTypes.STRING,
-    title: DataTypes.STRING,
-    imageUrl: DataTypes.STRING,
-    votes: DataTypes.INTEGER,
-    isFunny: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Meme',
-  });
-  return Meme;
+    Meme.init(
+        {
+            author: DataTypes.STRING,
+            title: DataTypes.STRING,
+            imageUrl: DataTypes.STRING,
+            votes: DataTypes.INTEGER,
+            isFunny: DataTypes.BOOLEAN,
+            CategoryId: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: "Meme",
+        }
+    );
+    return Meme;
 };
