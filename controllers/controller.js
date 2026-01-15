fdcda4d095aa858dc3bdaeb38abee79eedd10583
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-15 22:04:54 +0700

    R4: readCategories()

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,5 @@
+const { Meme, Category } = require("../models");
+
 class Controller {
     static async readMemes(req, res) {
         try {
@@ -8,6 +10,10 @@ class Controller {
     }
     static async readCategories(req, res) {
         try {
+            const categories = await Category.findAll();
+
+            res.render("categories", { categories });
+            // res.send(categories);
         } catch (error) {
             console.log(error);
             res.send(error);
